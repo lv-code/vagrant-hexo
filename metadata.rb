3622ed031464448f6123063edd2594ec8e5ea84e
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-10-12 16:25:30 +0200

    Prepares for Berkshelf

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              'vagrant-hexo'
+maintainer        'ALT3 B.V.'
+license           'MIT'
+description       'Vagrant provisioned Hexo blog server'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '1.0.0'
+
+supports 'centos'
+supports 'ubuntu'
